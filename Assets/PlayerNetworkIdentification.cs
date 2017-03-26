7bb86b8e31d80430fdd12cd2ae22015b35d3fbc9
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2017-03-27 01:12:03 +0300

    Fixed typo and Hamachi testing

--- Assets/PlayerNetworkIdentification.cs
+++ Assets/PlayerNetworkIdentification.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerNetworkIndetification : NetworkBehaviour {
+public class PlayerNetworkIdentification : NetworkBehaviour {
 
     public override void OnStartLocalPlayer()
     {
         GetComponent<PlayerMovement>().enabled = true;
         GetComponent<TestProjectile>().enabled = true;
+
+        GetComponent<SpriteRenderer>().color = Color.yellow;
+
+        Network.InitializeServer(32, 25000, false);
+        Network.Connect("25.60.115.205", 25000);
     }
 }
