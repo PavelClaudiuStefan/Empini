d0d522a9d56b8e0d6f97e2cd97fed8ab6c2b4aa1
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2017-04-05 16:57:38 +0300

    Added tree cutting
    Use space bar while near a special tree

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float closestResourceDistance;
+
+    private GameObject[] trees;
+    private GameObject closestResource;
+
+    void Start () {
+        
+    }
+	
+	
+	void Update () {
+ 
+        trees = GameObject.FindGameObjectsWithTag("Copac");
+        closestResource = GetClosestResource(trees);
+        closestResourceDistance = Vector3.Distance(closestResource.transform.position, transform.position);
+
+        if (Input.GetKeyDown("space") && closestResourceDistance < 0.6)
+        {
+            //Debug.Log(closestResource);
+            Debug.Log(closestResourceDistance);
+            Destroy(closestResource);
+        }
+        
+	}
+
+    GameObject GetClosestResource(GameObject[] resources)
+    {
+        GameObject tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject t in resources)
+        {
+            float dist = Vector3.Distance(t.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = t;
+                minDist = dist;
+            }
+        }
+        return tMin;
+    }
+
+
+}
